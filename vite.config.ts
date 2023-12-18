77e3ade1da0a016e92cb1d353b5c85e9f8aa9e13
Harry Ross <hazross@hotmail.com>
2023-12-18 16:05:49 +1100

    Removed old components

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -8,5 +9,26 @@ export default defineConfig({
     target: 'es2020',
     minify: false,
     copyPublicDir: false,
-  }
+    lib: {
+      entry: resolve(__dirname, "lib/index.tsx"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        }
+      }
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: resolve(__dirname, 'scripts/tests/setup.js'),
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 })
