b47d0b5c64fd8e42ed43288f86ab1d7902d364b7
Harry Ross <hazross@hotmail.com>
2023-12-22 17:09:16 +1100

    Fixed caching logic

--- lib/hooks/useMenuItems.ts
+++ lib/hooks/useMenuItems.ts
@@ -1,24 +1,45 @@
 import { useEffect, useState } from "react";
 import { NavMenuGroup } from "../types/megamenu";
 
+const DAY = 1000 * 60 * 60 * 24;
+
+const refreshData = async () => {
+  const res = await fetch("https://www.ssw.com.au/api/get-megamenu");
+
+  const json = await res.json();
+  if (window?.localStorage) {
+    const cached = {
+      value: json,
+      ttl: new Date().getTime() + DAY,
+    };
+    window.localStorage.setItem("megamenu", JSON.stringify(cached));
+  }
+
+  return json;
+};
+
 export const useMenuItems = (
   menuBarItems?: NavMenuGroup[],
 ): { menuItems: NavMenuGroup[] } => {
-  const [menuItems, setMenuItems] = useState<NavMenuGroup[]>([]);
+  const [menuItems, setMenuItems] = useState<NavMenuGroup[] | null>([]);
 
   useEffect(() => {
+    const cached = JSON.parse(window.localStorage.getItem("megamenu") || "{}");
+
     if (menuBarItems) {
       setMenuItems(menuBarItems);
-    } else if (window?.localStorage?.megamenu) {
-      setMenuItems(JSON.parse(window.localStorage.megamenu));
+    } else if (cached) {
+      if (cached.ttl < new Date().getTime()) {
+        refreshData().then((data) => {
+          setMenuItems(data);
+        });
+      } else {
+        setMenuItems(cached.value);
+      }
     } else {
-      fetch("https://www.ssw.com.au/api/get-megamenu")
-        .then((res) => res.json())
-        .then((json) => {
-          setMenuItems(json);
-          if (window) window.localStorage.megamenu = JSON.stringify(json);
-        })
-        .catch((err) => console.error(err));
+      refreshData().then((data) => {
+        setMenuItems(data);
+      });
     }
   }, [menuBarItems]);
 
