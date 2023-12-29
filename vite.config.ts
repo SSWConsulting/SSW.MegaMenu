42c274044182127d6eb4c0bc26485e0ff1a0455b
Harry Ross <hazross@hotmail.com>
2023-12-29 17:15:29 +1100

    Added types rollup

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     target: "esnext",
     minify: false,
