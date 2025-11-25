30bc03495350853505b6b959c2603bd86d3f4d73
Chloe Lin [SSW] <chloelin@ssw.com.au>
2025-11-25 13:34:43 +0100

    💄 Remove the px-1 padding from the hamburger button (#135)
    remove padding in hamburger

--- lib/components/ui/Divider.tsx
+++ lib/components/ui/Divider.tsx
@@ -2,5 +2,5 @@ import type { ClassValue } from "clsx";
 import { cx } from "../../util/cx";
 
 export const Divider = ({ className }: { className?: ClassValue }) => (
-  <div className={cx("h-4 w-px bg-gray-700/30", className)}></div>
+  <div className={cx("h-4 w-px bg-gray-700/30 mr-1", className)}></div>
 );
