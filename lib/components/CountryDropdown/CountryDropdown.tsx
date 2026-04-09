6fbb45114b6e6512a602e7909115cdec4302b275
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-04-09 15:21:34 -0700

    Update CountryDropdown.tsx

--- lib/components/CountryDropdown/CountryDropdown.tsx
+++ lib/components/CountryDropdown/CountryDropdown.tsx
@@ -51,7 +51,7 @@ const CountryDropdown = ({ url, className }: CountryDropdownProps) => {
     <Popover className={cx(className)}>
       <Popover.Button
         className={cx(
-          "flex items-center justify-center gap-x-1 rounded-md pl-4 py-1 text-sm font-semibold text-ssw-black outline-none xs:pr-4",
+          "flex items-center justify-center gap-x-1 rounded-md px-4 xl:pr-0 py-1 text-sm font-semibold text-ssw-black outline-none",
           "hover:scale-105",
         )}
         onClick={() => setIsOpened(!isOpened)}
